bbcbf7df19830541ca90d99385a3697daba9b27d
Sebastian Amezquita <samezquita2605@gmail.com>
2021-10-11 17:43:56 -0500

    initial commit

--- script.js
+++ script.js
@@ -1,25 +1,15 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
 const nav = document.getElementById('nav');
 const toggleIcon = document.getElementById('toggle-icon');
-const img1 = document.getElementById('img1');
-const img2 = document.getElementById('img2');
-const img3 = document.getElementById('img3');
 const textBox = document.getElementById('text-box');
 
 //Dark or light images
-function imageMode (color) {
-    img1.src = `img/undraw_code_review_${color}.svg`;
-    img2.src = `img/undraw_map_${color}.svg`;
-    img3.src = `img/undraw_feeling_proud_${color}.svg`;
-}
-
 function toggleDarkLightMode(isDark) {
     nav.style.backgroundColor = isDark ? 'rgb (0 0 0 / 50%)' : 'rgb(255 255 255 / 50%)';
     textBox.style.backgroundColor = isDark ? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50%)';
     toggleIcon.children[0].textContent = isDark ? 'Dark Mode' : 'Light Mode';
     isDark ? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon'):
         toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
-    isDark ? imageMode('dark') : imageMode('light');
 }
 
 //Switch theme dynamically
@@ -45,21 +35,4 @@ if (currentTheme) {
         toggleSwitch.checked = true;
         toggleDarkLightMode(true);
     }
-}
-
-//Slideshow
-var slideIndex = 1;
-showDivs(slideIndex);
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  x[slideIndex-1].style.display = "block";  
 }
